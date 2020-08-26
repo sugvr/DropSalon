f5a98a4c84f10d4f03f369a9444d5a1af8787ef8
sugvr <idisai115@gmail.com>
2020-08-26 00:07:13 -0400

    Arreglo de fotos al branch

--- dropsalon/src/Components/Portfolio/Portfolio.js
+++ dropsalon/src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import './Portfolio.css'
+import pic1 from "../../Assets/pic1.jpg";
+import pic2 from "../../Assets/pic2.jpg";
+import pic3 from "../../Assets/pic3.jpg";
+import pic4 from "../../Assets/pic4.jpg";
+import pic5 from "../../Assets/pic5.jpg";
+import pic6 from "../../Assets/pic6.jpg";
+import pic7 from "../../Assets/pic7.jpg";
+import pic8 from "../../Assets/pic8.jpg";
+
+
 function Portfolio() {
     return (
+      
         <div>
             <h1>Galeria de trabajos </h1>
             <div className="row">
@@ -10,48 +21,49 @@ function Portfolio() {
                     <div id="mdb-lightbox-ui"></div>
 
                     <div className="mdb-lightbox">
+                        
 
                         <figure className="col-md-4">
-    
-                                <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic1.jpg'} className="img-fluid" />
-                            
+                        
+                                <img alt="trabajo" src={pic1} className="img-fluid" />
+                        
                         </figure>
 
                         <figure className="col-md-4">
                            
-                                <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic2.jpg'} className="img-fluid" />
+                                <img alt="trabajo" src={pic2} className="img-fluid" />
                           
                         </figure>
 
                         <figure className="col-md-4">
                             
-                                <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic3.jpg'} className="img-fluid" />
+                                <img alt="trabajo" src={pic3} className="img-fluid" />
                           
                         </figure>
 
                         <figure className="col-md-4">
-                            <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic4.jpg'} className="img-fluid" />
+                            <img alt="trabajo" src={pic4} className="img-fluid" />
                            
                         </figure>
 
                         <figure className="col-md-4">
                            
-                                <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic5.jpg'} className="img-fluid" />
+                                <img alt="trabajo" src={pic5} className="img-fluid" />
                            
                         </figure>
 
                         <figure className="col-md-4">
-                               <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic6.jpg'} className="img-fluid" />
+                               <img alt="trabajo" src={pic6} className="img-fluid" />
                            
                         </figure>
 
                         <figure className="col-md-4">
-                             <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic7.jpg'} className="img-fluid" />
+                             <img alt="trabajo" src={pic7} className="img-fluid" />
                            
                         </figure>
 
                         <figure className="col-md-4">
-                              <img alt="picture" src={process.env.PUBLIC_URL + '/images/pic8.jpg'} className="img-fluid" />
+                              <img alt="foto" src={pic8} className="img-fluid" />
                            
                         </figure>
 
