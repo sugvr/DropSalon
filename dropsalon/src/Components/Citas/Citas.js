88e5721257a3006b2f108d0c4facd2147d79bc77
sugvr <idisai115@gmail.com>
2020-08-31 12:54:29 -0400

    empeze con el login area

--- dropsalon/src/Components/Citas/Citas.js
+++ dropsalon/src/Components/Citas/Citas.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Citas.css'
 
 function Citas() {
     return (
-        <div>
+        <div className = "login">
+              <div className ="caja2">
+                <blockquote>
+                <h5>Les recuerdo, <br/>si aun no tiene una cuenta tiene que comunicarse con nosotros para asi poder crearla. Asi podra sacar su cita.</h5>
+                </blockquote>
+                
+            </div>
+            <div className ="caja1">
+            <form >
+                <div class="form-group">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" />
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password" />
+                </div>
+               
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+            </form>
+            </div>
           
         </div>
     )
