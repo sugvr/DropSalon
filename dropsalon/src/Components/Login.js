999ec00931bf9eb08d1fa89dd222db27d8fe0309
sugvr <idisai115@gmail.com>
2020-09-20 20:55:22 -0400

    up to date

--- dropsalon/src/Components/Login.js
+++ dropsalon/src/Components/Login.js
@@ -6,7 +6,7 @@ import SignUp from './SignUp'
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {valueEmail: '', valuePassword: '', SignUp: 'none', Login: 'block'};
+    this.state = { valueEmail: '', valuePassword: '', SignUp: 'none', Login: 'block' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,13 +14,13 @@ class Login extends React.Component {
 
   }
   toggle = () => {
-   if(this.state.SignUp === 'none' ) {
-     this.setState({SignUp:"block"})
-     this.setState({Login:"none"})
-   }else {
-     this.setState({SignUp:"none"})
-     this.setState({Login:"block"})
-   }
+    if (this.state.SignUp === 'none') {
+      this.setState({ SignUp: "block" })
+      this.setState({ Login: "none" })
+    } else {
+      this.setState({ SignUp: "none" })
+      this.setState({ Login: "block" })
+    }
   }
   handleChange(event) {
     const name = event.target.name;
@@ -35,41 +35,41 @@ class Login extends React.Component {
       url: 'http://localhost:4000/login',
       data: {
         email: this.state.valueEmail,
-        password:this.state.valuePassword
+        password: this.state.valuePassword
       }
     })
 
   }
-render(){
-  return (
+  render() {
+    return (
       <div>
-      <h1>Entra a tu cuenta</h1>
-      <form className="flex" onSubmit={this.handleSubmit}>
-        <div className="cajon1" style={{display:this.state.Login}}>
-          <label>
-            Email
-          <input name="valueEmail" type="email" placeholder="Email" onChange={this.handleChange}/>
-          </label>
-        </div>
-        <div className="cajon2"  style={{display:this.state.Login}}>
-          <label>
-            Password
-          <input name="valuePassword" type="password" placeholder="Password" onChange={this.handleChange}/>
-          </label>
-        </div>
-        <div className="cajon3"  style={{display:this.state.Login}}>
-          <button type="submit">Entra</button>
-          <button className="toggle" type="button" onClick={this.toggle}>Registra tu cuenta</button>
-        </div>
-       
-        <SignUp visible={this.state.SignUp} />
-      </form>
-      
-     
-    </div>
+        <h1>Entra a tu cuenta</h1>
+        <form className="flex" onSubmit={this.handleSubmit}>
+          <div className="cajon1" style={{ display: this.state.Login }}>
+            <label>
+              Email
+          <input name="valueEmail" type="email" placeholder="Email" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="cajon2" style={{ display: this.state.Login }}>
+            <label>
+              Password
+          <input name="valuePassword" type="password" placeholder="Password" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="cajon3" style={{ display: this.state.Login }}>
+            <button type="submit">Entra</button>
+            <button className="toggle" type="button" onClick={this.toggle}>Registra tu cuenta</button>
+          </div>
 
-  );
-};
+          <SignUp visible={this.state.SignUp} />
+        </form>
+
+
+      </div>
+
+    );
+  };
 
 }
 
